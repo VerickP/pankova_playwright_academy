f15653cb54a44f5d4dfaedbbb4ed729b65973668
Veronika Pánková <pankova.veri@gmail.com>
2025-12-12 19:12:46 +0100

    Add github actions test

---
+++ assets/dictionaries/dictionary.ts
@@ -0,0 +1,14 @@
+export const pmtoolTexts = {
+  login: {
+    title: "Login",
+    usernamePlaceholder: "Username",
+    rememberMe: "Remember Me",
+    lostPassword: "Password Forgotten?",
+    loginButton: "Login",
+  },
+  dashboard: {
+    appName: "TEG Project Management",
+  },
+  projects: {},
+  general: {},
+};
